c3622dedfe259eb94fa9c6bb0b4d276224b25a91
ni14o9 <115477691+ni14o9@users.noreply.github.com>
2022-11-07 08:42:51 -0300

    javascript

---
+++ 1.js
@@ -0,0 +1,11 @@
+var horas; 
+var minutos;
+var segundos_restantes;
+var segundos_totais = parseInt(prompt("Segundos?"));
+
+horas =parseInt(segundos_totais / 3600);
+segundos_restantes = segundos_totais - (horas * 3600);
+minutos =parseInt(segundos_restantes / 60); 
+segundos_restantes = segundos_restantes - (minutos * 60);0 
+
+alert(horas+"H : "+ )
